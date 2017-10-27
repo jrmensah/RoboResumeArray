53e9f6ae4f5e828c3a37e04934af8e98e1938566
jrmensah <jerilynmensah@gmail.com>
2017-10-27 16:48:15 -0400

    got all values to print at the end.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -54,77 +54,72 @@ public class Main {
         System.out.println("Please enter your name, email, education, work experiences and skills with the rating below");
         System.out.println(" ");
 
+    do {
+        System.out.println("Enter your Name:");
+        person.setName(keyboard.nextLine());
+        System.out.println("Enter your Email:");
+        person.setName(keyboard.nextLine());
+        System.out.println("Education Achievements:");
+        System.out.println(" ");
+        System.out.println("Please submit between 1 to 10 entries.");
 
-        do {
-            System.out.println("Enter your Name:");
-            person.setName(keyboard.nextLine());
-            System.out.println("Enter your Email:");
-            person.setName(keyboard.nextLine());
-            System.out.println("Education Achievements:");
-            System.out.println(" ");
-            System.out.println("Please submit between 1 to 10 entries.");
+        for (counter = 0; counter <= 10; counter++) {
 
-            for (counter = 1; counter <= 10; counter++) {
-                do {
-                    System.out.println("You have submitted" + " " + (counter) + " " + "entries");
-                    System.out.println(" ");
-                    System.out.println("Enter the Degree of College");
-                    college.degree = keyboard.nextLine();
-                    System.out.println("Enter Major at College:");
-                    college.major = keyboard.nextLine();
-                    System.out.println("Enter Name of College:");
-                    college.name = keyboard.nextLine();
-                    System.out.println("Enter the year of Graduation");
-                    college.year = keyboard.nextInt();
-                    keyboard.nextLine();
-                    education.add(college);
-                    System.out.println("Do you want to enter a school?");
+
+            do {
+
+                System.out.println("You have submitted" + " " + (counter) + " " + "entries");
+                System.out.println(" ");
+                System.out.println("Enter the Degree of College");
+                college.degree = keyboard.nextLine();
+                System.out.println("Enter Major at College:");
+                college.major = keyboard.nextLine();
+                System.out.println("Enter Name of College:");
+                college.name = keyboard.nextLine();
+                System.out.println("Enter the year of Graduation");
+                college.year = keyboard.nextInt();
+                keyboard.nextLine();
+                education.add(college);
+                System.out.println("Do you want to enter a school?");
+                answer = keyboard.nextLine();
+                if (!answer.equalsIgnoreCase("y") && !answer.equalsIgnoreCase("n")) {
+                    System.out.println("Invalid entry. Please enter 'Y' or 'N'");
                     answer = keyboard.nextLine();
-                    if (!answer.equalsIgnoreCase("y") && !answer.equalsIgnoreCase("n")) {
-                        System.out.println("Invalid entry. Please enter 'Y' or 'N'");
-                        answer = keyboard.nextLine();
-                    }
-
-                }while (answer.equalsIgnoreCase("y") || !answer.equalsIgnoreCase("n"));
-                if (counter == 0)
-                {
-                    System.out.println("You must submit at least 1 entry!");
-                } else if (counter > 10)
-                {
-                    System.out.println("You have exceeded 10 entries!");
                 }
-
-                System.out.println("You have submitted" + " " + (counter + 1) + " " + "entries");
-                System.out.println(" ");
-
-                //Print all college entries
-                System.out.println("Here are the education achievements you entered:");
-                System.out.println(" ");
-
-                for (College colleges : education) {
-                    System.out.print(college.degree);
-                    System.out.print(" " + "in" + " ");
-                    System.out.println(college.major);
-                    System.out.print(college.name);
-                    System.out.println(" " + "," + college.year);
+            }while (answer.equalsIgnoreCase("y") || !answer.equalsIgnoreCase("n"));
 
 
-                }
-                System.out.println(" ");
+            System.out.println("You have submitted" + " " + (counter+=1) + " " + "entries");
+            System.out.println(" ");
+
+            //Print all college entries
+            System.out.println("Here are the education achievements you entered:");
+            System.out.println(" ");
+
+            for (College colleges : education) {
+                System.out.print(college.degree);
+                System.out.print(" " + "in" + " ");
+                System.out.println(college.major);
+                System.out.print(college.name);
+                System.out.println(" " + "," + college.year);
+
+
             }
-            while(answer.equalsIgnoreCase("y")) ;
+
+        System.out.println(" ");
+
 
 
         // Get the user's work experience
-            for(counter = 0; counter <= 10; counter++) {
+        for (counter = 0; counter <= 10; counter++) {
+            do {
+                System.out.println("Please submit between 0 to 10 entries.");
+                System.out.println("");
+                if (counter > 10) {
+                    System.out.println("You have exceeded 10 entries");
+                }
                 do {
-                    System.out.println("Please submit between 0 to 10 entries.");
-                    System.out.println("");
-                    if (counter > 10) {
-                        System.out.println("You have exceeded 10 entries");
-                    }
-                    do{
-                        System.out.println("You have submitted" + " " + (counter) + " " + "entries");
+                    System.out.println("You have submitted" + " " + (counter) + " " + "entries");
                     System.out.println("Enter the job title");
                     job.title = keyboard.nextLine();
                     System.out.println("Enter the place of the job:");
@@ -143,7 +138,7 @@ public class Main {
                         answer = keyboard.nextLine();
                     }
                 } while (answer.equalsIgnoreCase("y") || !answer.equalsIgnoreCase("n"));
-                System.out.println("You have submitted" + " " + (counter+1) + " " + "entries");
+                System.out.println("You have submitted" + " " + (counter+=1) + " " + "entries");
                 System.out.println(" ");
 
                 //Print all job entries
@@ -156,82 +151,95 @@ public class Main {
                     System.out.println(job.duty1);
                     System.out.println(job.duty2);
                 }
-            }while(answer.equalsIgnoreCase("y"));
 
-            //Get the user's Skills and their rating
 
-            for(counter = 1; counter <= 20; counter++)
-            {
-            do {
-                System.out.println("Please submit between 1 to 20 entries");
-                System.out.println("");
-                if( counter < 1){
-                    System.out.println("Please submit at least 1 entry!");
-                }else if(counter > 20){
-                    System.out.println("You have exceeded 20 entries!");
-                }
-                do{
-                    System.out.println("You have submitted" + " " + (counter) + " " + "entries");
+                //Get the user's Skills and their rating
+
+                for (counter = 1; counter <= 20; counter++) {
+                    do {
+                        System.out.println("Please submit between 1 to 20 entries");
+                        System.out.println("");
+                        if (counter < 1) {
+                            System.out.println("Please submit at least 1 entry!");
+                        } else if (counter > 20) {
+                            System.out.println("You have exceeded 20 entries!");
+                        }
+                        do {
+                            System.out.println("You have submitted" + " " + (counter) + " " + "entries");
+                            System.out.println(" ");
+
+                            System.out.println("Enter your first skill");
+                            skill.skill1 = keyboard.nextLine();
+                            System.out.println("Enter your second skill:");
+                            skill.skill2 = keyboard.nextLine();
+                            System.out.println("Enter your third skill:");
+                            skill.skill3 = keyboard.nextLine();
+                            expertise.add(skill);
+                            System.out.println("Do you want to enter a set of skills?");
+                            answer = keyboard.nextLine();
+                            if (!answer.equalsIgnoreCase("y") && !answer.equalsIgnoreCase("n")) {
+                                System.out.println("Invalid entry. Please enter 'Y' or 'N'");
+                                answer = keyboard.nextLine();
+                            }
+                        } while (answer.equalsIgnoreCase("y") || !answer.equalsIgnoreCase("n"));
+                        System.out.println("You have submitted" + " " + (counter+=3) + " " + "entries");
+                        System.out.println(" ");
+
+
+                        //Print all skills with ratings
+                        System.out.println("Here are the skills and ratings you entered:");
+                        System.out.println(" ");
+
+                        for (Skill skills : expertise) {
+                            System.out.println(skill.skill1);
+                            System.out.println(skill.skill2);
+                            System.out.println(skill.skill3);
+
+                        }
+                    } while (answer.equalsIgnoreCase("y"));
+                    // Print all variables
+                    System.out.println("");
+                    System.out.println(person.getName());
+                    System.out.println(person.getEmail());
+                    System.out.println("");
+                    System.out.println("Education");
+                    System.out.print(college.degree);
+                    System.out.print(" " + "in" + " ");
+                    System.out.println(college.major);
+                    System.out.print(college.name);
+                    System.out.println(" " + "," + college.year);
                     System.out.println(" ");
+                    System.out.println("Experience");
+                    System.out.println(job.title);
+                    System.out.println(job.place + " " + "," + " " + " " + job.date);
+                    System.out.println(job.duty1);
+                    System.out.println(job.duty2);
+                    System.out.println(" ");
+                    System.out.println("Skills");
+                    System.out.println(skill.skill1);
+                    System.out.println(skill.skill2);
+                    System.out.println(skill.skill3);
 
-                System.out.println("Enter your first skill");
-                skill.skill1 = keyboard.nextLine();
-                System.out.println("Enter your second skill:");
-                skill.skill2 = keyboard.nextLine();
-                System.out.println("Enter your third skill:");
-                skill.skill3 = keyboard.nextLine();
-                expertise.add(skill);
-                System.out.println("Do you want to enter a set of skills?");
-                answer = keyboard.nextLine();
-                if (!answer.equalsIgnoreCase("y") && !answer.equalsIgnoreCase("n")) {
-                    System.out.println("Invalid entry. Please enter 'Y' or 'N'");
-                    answer = keyboard.nextLine();
                 }
-                } while (answer.equalsIgnoreCase("y") || !answer.equalsIgnoreCase("n"));
-            System.out.println("You have submitted" + " " +(counter+1)+ " " +"entries");
-            System.out.println(" ");
+                while (answer.equalsIgnoreCase("y")) ;
+            } while (answer.equalsIgnoreCase("y"));
+        }
 
-
-            //Print all skills with ratings
-            System.out.println("Here are the skills and ratings you entered:");
-            System.out.println(" ");
-
-            for (Skill skills : expertise) {
-                System.out.println(skill.skill1);
-                System.out.println(skill.skill2);
-                System.out.println(skill.skill3);
-
-            }
-
-            // Print all variables
-            System.out.println("");
-            System.out.println(person.getName());
-            System.out.println(person.getEmail());
-            System.out.println("");
-            System.out.println("Education");
-            System.out.print(college.degree);
-            System.out.print(" " + "in" + " ");
-            System.out.println(college.major);
-            System.out.print(college.name);
-            System.out.println(" " + "," + college.year);
-            System.out.println(" ");
-            System.out.println("Experience");
-            System.out.println(job.title);
-            System.out.println(job.place + " " + "," + " " + " " + job.date);
-            System.out.println(job.duty1);
-            System.out.println(job.duty2);
-            System.out.println(" ");
-            System.out.println("Skills");
-            System.out.println(skill.skill1);
-            System.out.println(skill.skill2);
-            System.out.println(skill.skill3);
-
-        } while (answer.equalsIgnoreCase("y"));
+        }
+    }while(answer.equalsIgnoreCase("y"));
     }
 }
-        }while(answer.equalsIgnoreCase("y"));
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
